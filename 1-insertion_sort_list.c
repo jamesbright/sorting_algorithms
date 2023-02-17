ad995ae47552bf40030e6f7926a2a07f8eb97c74
James Ononiwu <jamesononiwu@gmail.com>
2023-02-17 01:49:24 -0800

    vim sort.h

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function sorts a list with insertion sort
+ * @list: doubly linked list to sort
+ *
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+bool flag = false;
+listint_t *tmp = NULL, *aux = NULL;
+
+if (!list || !(*list) || !(*listt)->next)
+return;
+
+tmp = *list;
+
+while (tmp->next)
+{
+if (tmp->n > tmp->next->n)
+{
+tmp->next->prev = tmp->prev;
+if (tmp->next->prev)
+tmp->prev->next = tmp->next;
+else
+*list = tmp->next;
+
+tmp->prev = tmp->next;
+tmp->next = tmp->next->next;
+tmp->prev->next = tmp;
+if (tmp->next)
+tmp->next->prev = tmp;
+
+tmp = tmp->prev;
+print_list(*list);
+
+if (tmp->prev && tmp->prev->n > tmp->n)
+{
+if (!flag)
+aux = tmp->next;
+flag = true;
+tmp = tmp->prev;
+continue;
+}
+}
+if (!flag)
+tmp = tmp->next;
+else
+tmp = aux, flag = false;
+}
--- sort.h
+++ sort.h
@@ -6,6 +6,7 @@
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 /**
  * struct listint_s - Doubly linked list node
