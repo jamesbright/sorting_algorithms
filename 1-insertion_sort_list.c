3538a220e95b6651a95a91cfec0e7c7a9d995ca1
James Ononiwu <jamesononiwu@gmail.com>
2023-02-18 06:10:16 -0800

    updated

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,8 @@ void insertion_sort_list(listint_t **list)
 {
 bool flag = false;
 listint_t *tmp = NULL, *aux = NULL;
-if (!list || !(*list) || !(*list)->next) return;
+if (!list || !(*list) || !(*list)->next)
+return;
 tmp = *list;
 while (tmp->next)
 {
@@ -18,22 +19,24 @@ if (tmp->n > tmp->next->n)
 tmp->next->prev = tmp->prev;
 if (tmp->next->prev)
 tmp->prev->next = tmp->next;
-else *list = tmp->next;
-tmp->prev = tmp->next;
-tmp->next = tmp->next->next;
+else
+*list = tmp->next;
+tmp->prev = tmp->next, tmp->next = tmp->next->next;
 tmp->prev->next = tmp;
-if (tmp->next) tmp->next->prev = tmp;
-tmp = tmp->prev;
+if (tmp->next)
+tmp->next->prev = tmp, tmp = tmp->prev;
 print_list(*list);
 if (tmp->prev && tmp->prev->n > tmp->n)
 {
-if (!flag) aux = tmp->next;
-flag = true;
-tmp = tmp->prev;
+if (!flag)
+aux = tmp->next;
+flag = true, tmp = tmp->prev;
 continue;
 }
 }
-if (!flag) tmp = tmp->next;
-else tmp = aux, flag = false;
+if (!flag)
+tmp = tmp->next;
+else
+tmp = aux, flag = false;
 }
 }
