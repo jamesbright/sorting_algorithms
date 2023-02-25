857512576451938812e1350c76fbe27d6894813a
James Ononiwu <jamesononiwu@gmail.com>
2023-02-25 10:06:02 -0800

    quick sort hoare

--- sort.h
+++ sort.h
@@ -43,4 +43,5 @@ int split(int *arr, int left, int right, size_t size);
 void swap(listint_t **head, listint_t *node1, listint_t *node2);
 void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 #endif /** END _SORT_H_ **/
